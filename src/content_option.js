55b2180425be5147e4bcf385d0df1a422ecea69b
desotvh <berendicto@gmail.com>
2024-04-14 15:24:34 -0400

    Primer upload de portfolio

---
+++ src/content_option.js
@@ -0,0 +1,190 @@
+const logotext = "SF";
+const meta = {
+  title: "Seba Flores",
+  description:
+    "Hola! Soy Sebastian Flores, un apasionado por la ciencia, la vida, la logica, el deporte, el estilo y el trabajo en equipo.",
+};
+
+const introdata = {
+  title: "Hola! Soy Seba Flores.",
+  animated: {
+    first: "Soy un desarrollador web.",
+    second: "Desarrollo paginas y aplicaciones web geniales.",
+    third: "Estoy para ayudarte.",
+    fourth: "I LOVE CODING.",
+  },
+  description:
+    "Bienvenido a mi portafolio online. Soy Sebastian Flores, un entusiasta del deporte, la ciencia y la programación. Este espacio es mi lienzo digital, donde combino mi pasión por la tecnología con mi creatividad. Aunque recién estoy comenzando mi viaje en el mundo del desarrollo web, estoy emocionado por lo que el futuro me depara. Únete a mí mientras exploramos juntos las posibilidades del diseño web y las aplicaciones. ¡Juntos podemos crear algo asombroso!",
+  your_img_url: "https://r2.easyimg.io/d9xlhka4j/perdilportfolio.png",
+};
+
+const dataabout = {
+  title: "Un poco sobre mi",
+  aboutme:
+    "Soy Sebastian Flores, un apasionado de la ciencia y la tecnología, originario de Iquique. Desde temprana edad, he sentido una fascinación innata por explorar cómo la tecnología puede transformar nuestro mundo. Esta pasión me llevó a cursar una carrera como técnico en automatización y control industrial hace dos años. Sin embargo, mi amor por la programación me impulsó a continuar mi formación y titularme en el campo del desarrollo web. Disfruto del trabajo colaborativo y creo firmemente en el poder del equipo para lograr grandes cosas. Mi enfoque en el desarrollo web no solo se centra en escribir código, sino en crear experiencias digitales impactantes y funcionales. Estoy emocionado por seguir aprendiendo y creciendo en este apasionante campo, explorando nuevas tecnologías y contribuyendo al mundo digital de manera significativa.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Frontend Developer",
+    where: "Job Tracker Project - DesafioLatam Academy",
+    date: "2024",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "Jamalya",
+    date: "2019",
+  },
+  {
+    jobtitle: "Designer of week",
+    where: "ALquds",
+    date: "2019",
+  },
+];
+
+const skills = [
+  {
+    name: "Javascript",
+    value: 90,
+  },
+  {
+    name: "SQL",
+    value: 85,
+  },
+  {
+    name: "React",
+    value: 80,
+  },
+  {
+    name: "Tailwind CSS",
+    value: 60,
+  },
+  {
+    name: "Node.JS",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "UI & UX Diseño",
+    description:
+      "Ofrezco servicios de diseño de UX/UI centrados en la experiencia del usuario y la interfaz intuitiva. Desde la creación de wireframes hasta el diseño visual, me especializo en crear interfaces atractivas y funcionales que maximizan la usabilidad. Con un enfoque centrado en el usuario, transformo ideas en experiencias digitales cautivadoras que destacan la identidad de tu marca y mejoran la interacción con tus productos o servicios.",
+  },
+  {
+    title: "Aplicaciones WEB",
+    description:
+      "Ofrezco servicios de desarrollo de aplicaciones web personalizadas y de alta calidad. Desde el diseño conceptual hasta la implementación y la optimización, me especializo en crear soluciones web robustas y escalables. Con un enfoque en la usabilidad y el rendimiento, desarrollo aplicaciones que no solo cumplen con tus necesidades, sino que también proporcionan una experiencia fluida y satisfactoria para los usuarios.",
+  },
+  {
+    title: "Diseño Paginas WEB",
+    description:
+      "Ofrezco servicios completos de diseño y desarrollo de páginas web adaptadas a tus necesidades específicas. Desde la creación de diseños atractivos y funcionales hasta la implementación de soluciones tecnológicas avanzadas, mi objetivo es crear sitios web que cautiven a tus visitantes desde el primer momento. Con un enfoque en la estética, la usabilidad y la accesibilidad, construyo páginas web que reflejan la identidad de tu marca y optimizan la experiencia del usuario. ¡Déjame ayudarte a hacer brillar tu presencia en línea!",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://desotvh.github.io/PortfolioFinal/assets/images/cableado.png",
+    description: "Cableado estructurado en espacios de la mineria.",
+    link: "https://desotvh.github.io/fdsw-github/",
+  },
+  {
+    img: "https://desotvh.github.io/PortfolioFinal/assets/images/tundrawolfspec.png",
+    description: "La naturaleza de los lobos.",
+    link: "https://desotvh.github.io/iguanapage/",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "sebafloresyavar@outlook.com",
+  YOUR_FONE: "+56975268206",
+  description:
+    "¡Hablemos! Estoy aquí para ayudarte. Por favor, completa el siguiente formulario y me pondré en contacto contigo lo antes posible. Tu opinión es importante para mí y estoy emocionado de escuchar sobre tus proyectos y cómo puedo contribuir a su éxito.",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/DEsoTVH",
+  facebook: "https://web.facebook.com/fvckyov/",
+  linkedin: "https://www.linkedin.com/in/sebasti%C3%A1n-flores-533868a3/",
+  twitter: "https://twitter.com/fuckurg0d",
+  twitch: "https://www.twitch.tv/desotvh",
+  instagram: "https://www.instagram.com/tatanewblood",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
